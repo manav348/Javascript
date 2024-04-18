1dcc5933c9dc66a67b9f49df6fa65ba24d0c25fb
Manav Mahendru <mmahendru348@gmail.com>
2024-04-18 12:02:57 +0530

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello")
+// alert(3+3) /*We are using nodeJS not browser
+console.log(3+3) //code readability should be high
+console.log("Hitesh")
+
+let name="Hitesh"
+let age=18
+let isLoggedIn= false
+
+//Datatypes
+//int ->[2**53]
+//bigint
+//string->" "
+//bool->true/false
+//null->standalone value
+//undefined
+//symbol->unique
+//object
+console.log(typeof("hitesh"));
+console.log(typeof(age));
+console.log(typeof null);
+console.log(typeof undefined);
+
