6838bc837985d6c5aae348027d31828417e0cdf1
Manav Mahendru <mmahendru348@gmail.com>
2024-04-18 12:28:31 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+//Number
+/*let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score1=null
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+let score2=undefined
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let score3=true
+let valueInNumber3=Number(score3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3)
+
+
+let score4="manav"
+let valueInNumber4=Number(score4)
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4)
+*/
+//"33"->33
+//"33abc"->NaN
+//true->1 false->0
+
+//Bool
+/*let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedIn1="manav"
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1);
+
+let isLoggedIn2=""
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2);
+*/
+//1->true; 0->false
+//""->false; "manav"->true
+
+//string
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
